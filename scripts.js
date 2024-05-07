9e8e8e757174b79cbb66b76e81dc569d6b5b5018
OnaBenedi <onabenedi@gmail.com>
2024-05-07 12:04:51 +0200

    added some script + fixed html, currently working on language and region menus

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const languageButton = document.getElementById("language");
+const languageMenu =  document.getElementById("language-menu");
+
+function showLanguageClass(){
+    languageMenu.classList.toggle("language-visible")
+}
+
+// languageButton.addEventListener("mouseover", changeLanguageClass() )
