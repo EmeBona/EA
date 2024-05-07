20b1579c65b25f940c78f8248798c8ea45dfaedc
OnaBenedi <onabenedi@gmail.com>
2024-05-07 15:18:23 +0200

    added functionality for bottom buttons, fixed footer menus html + css

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const regionButton = document.getElementById("region");
+const regionMenu = document.getElementById("region-menu");
 const languageButton = document.getElementById("language");
 const languageMenu =  document.getElementById("language-menu");
 
@@ -5,4 +7,7 @@ function showLanguageClass(){
     languageMenu.classList.toggle("language-visible")
 }
 
+function showRegionClass(){
+    regionMenu.classList.toggle("region-visible")
+}
 // languageButton.addEventListener("mouseover", changeLanguageClass() )
