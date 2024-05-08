b262d64e649529ebef900acd42544322070b3595
OnaBenedi <onabenedi@gmail.com>
2024-05-08 16:25:39 +0200

    added js function for footer chevrons

--- scripts.js
+++ scripts.js
@@ -2,12 +2,36 @@ const regionButton = document.getElementById("region");
 const regionMenu = document.getElementById("region-menu");
 const languageButton = document.getElementById("language");
 const languageMenu =  document.getElementById("language-menu");
+const chevronRegion = document.getElementById("chevron-region")
+const chevronLanguage = document.getElementById("chevron-language")
 
-function showLanguageClass(){
-    languageMenu.classList.toggle("language-visible")
-}
+
 
 function showRegionClass(){
     regionMenu.classList.toggle("region-visible")
 }
-// languageButton.addEventListener("mouseover", changeLanguageClass() )
+function changeRegionChevron(){
+    if(chevronRegion.className === "fa-solid fa-chevron-down"){
+        chevronRegion.classList.replace("fa-chevron-down", "fa-chevron-up")
+    } else if (chevronRegion.className === "fa-solid fa-chevron-up"){
+        chevronRegion.classList.replace("fa-chevron-up", "fa-chevron-down")
+    }
+    
+}
+
+function showLanguageClass(){
+    languageMenu.classList.toggle("language-visible")
+}
+function changeLanguageChevron(){
+    if(chevronLanguage.className === "fa-solid fa-chevron-down"){
+        chevronLanguage.classList.replace("fa-chevron-down", "fa-chevron-up")
+    } else if (chevronLanguage.className === "fa-solid fa-chevron-up"){
+        chevronLanguage.classList.replace("fa-chevron-up", "fa-chevron-down")
+    }
+    
+}
+regionButton.addEventListener("click", showRegionClass)
+regionButton.addEventListener("click", changeRegionChevron)
+
+languageButton.addEventListener("click", showLanguageClass)
+languageButton.addEventListener("click", changeLanguageChevron)
